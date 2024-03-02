4ead08a10cf9bce97b2d40612c8e78429191fe28
Denis Maslov <deniska-maslov@bk.ru>
2024-03-02 05:54:49 +0300

    Урок 2

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
-let = 'money';
-let = 'income';
-let = 'addExpenses';
-let = 'deposit';
-let = 'mission ';
-alert('Любой текст');
-console.log('Любой текст');
+let money = 1000;
+let income = 500;
+let addExpenses = "100, 200, 300";
+let deposit =  true;
+let mission = 10000;
+let period = 5;
+
+console.log (typeof money , typeof income , typeof deposit);
+console.log (addExpenses.length);
+console.log ('Период равен',period,'месяцев');
+console.log ('Цель заработать',mission,'рублей/долларов/гривен/юани');
+
+addExpenses = addExpenses.toLowerCase ();
+addExpenses = addExpenses.split(',');
+console.log(addExpenses)
+
+const budgetDay = money / 30;
+console.log (budgetDay);
