c5d7d326cb4a30698c16675825252d7d3073025e
Denis Maslov <deniska-maslov@bk.ru>
2024-03-02 06:26:51 +0300

    Сложный урок намба ту

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let num = 266219;
+num = num.toString();
+num = +num[0] * +num[1] * +num[2] * +num[3] * +num[4] * +num[5];
+console.log (num);
+
+num = num ** 3;
+console.log (num);
+
+num = num.toString();
+console.log(num[0]+num[1]);
