38300f6f43af38a377da5024d86bf9bf76ee1dc7
PASHANOJ <158532470+PASHANOJ@users.noreply.github.com>
2024-03-03 09:52:03 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,9 @@
 const money = 1000;
 const income = 'фриланс';
-let addExpenses = "100, 200, 300";
+const addExpenses = "100, 200, 300";
 const deposit =  true;
 const mission = 10000;
 const period = 5;
 
-console.log(typeof money , typeof income , typeof deposit);
-console.log(addExpenses.length);
-console.log('Период равен',period,'месяцев');
-console.log('Цель заработать',mission,'рублей/долларов/гривен/юани');
-
-addExpenses = addExpenses.toLowerCase ();
-addExpenses = addExpenses.split(',');
-console.log(addExpenses)
-
-const budgetDay = money / 30;
-console.log(budgetDay);
+alert(income);
+console.log(income);
