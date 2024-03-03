3e39ecd7c46d6770578bddc6109cf7efa9909f53
PASHANOJ <158532470+PASHANOJ@users.noreply.github.com>
2024-03-03 11:17:00 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 const money = 1000;
 const income = 'фриланс';
-const addExpenses = "100, 200, 300";
+const addExpenses = '100, 200, 300';
 const deposit =  true;
 const mission = 10000;
 const period = 5;
