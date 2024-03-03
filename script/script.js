f82a3e5b158985569d38a73a05aae713b016b6c8
Denis Maslov <deniska-maslov@bk.ru>
2024-03-03 09:16:59 +0300

    fix

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
-let money = 1000;
-let income = 500;
+const money = 1000;
+const income = 'фриланс';
 let addExpenses = "100, 200, 300";
-let deposit =  true;
-let mission = 10000;
-let period = 5;
+const deposit =  true;
+const mission = 10000;
+const period = 5;
 
-console.log (typeof money , typeof income , typeof deposit);
-console.log (addExpenses.length);
-console.log ('Период равен',period,'месяцев');
-console.log ('Цель заработать',mission,'рублей/долларов/гривен/юани');
+console.log(typeof money , typeof income , typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен',period,'месяцев');
+console.log('Цель заработать',mission,'рублей/долларов/гривен/юани');
 
 addExpenses = addExpenses.toLowerCase ();
 addExpenses = addExpenses.split(',');
 console.log(addExpenses)
 
 const budgetDay = money / 30;
-console.log (budgetDay);
+console.log(budgetDay);
