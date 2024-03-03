ac8d20caa21a87129cba27fce5ff1e8a32f49e0b
PASHANOJ <158532470+PASHANOJ@users.noreply.github.com>
2024-03-03 15:41:24 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,32 @@
-const money = 1000;
+'use strict'
+const money = prompt('Ваш месячный доход?');
 const income = 'фриланс';
-const addExpenses = '100, 200, 300';
-const deposit =  true;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+const deposit =  confirm('Есть ли у вас депозит в банке?');
 const mission = 10000;
 const period = 5;
+const expenses1 = prompt('Введите обязательную статью расходов?');
+const amount1 = prompt('Во сколько это обойдется?');
+const expenses2 = prompt('Введите обязательную статью расходов?');
+const amount2 = prompt('Во сколько это обойдется?');
 
-alert(income);
-console.log(income);
+addExpenses = addExpenses.split(',');
+const addExpensesSum = addExpenses.reduce((partialSum,currentValue) => +partialSum + +currentValue ,0);
+const budgetMonth = money - amount1 - amount2 - addExpensesSum; 
+console.log(budgetMonth);
+
+const missionTime = Math.ceil(mission / budgetMonth);
+console.log(missionTime);
+
+const budgetDay = Math.floor(budgetMonth / 30);
+console.log(budgetDay)
+
+if(budgetDay>1200){
+    console.log('У вас высокий уровень дохода');
+} else if (budgetDay>= 600 && budgetDay<=1200) {
+    console.log('У вас средний уровень дохода');
+} else if (budgetDay<600 && budgetDay>=0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+    console.log('Что-то пошло не так');
+}
