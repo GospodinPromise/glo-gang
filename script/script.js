2a14d2c3c9a2e2847237f3ee5adacb4c3df483ff
PASHANOJ <158532470+PASHANOJ@users.noreply.github.com>
2024-03-04 07:49:36 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,32 +1,32 @@
+// ПЕРВОЕ ЗАДАНИЕ
+
 'use strict'
-const money = prompt('Ваш месячный доход?');
-const income = 'фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-const deposit =  confirm('Есть ли у вас депозит в банке?');
-const mission = 10000;
-const period = 5;
-const expenses1 = prompt('Введите обязательную статью расходов?');
-const amount1 = prompt('Во сколько это обойдется?');
-const expenses2 = prompt('Введите обязательную статью расходов?');
-const amount2 = prompt('Во сколько это обойдется?');
+const language = confirm('ДА - RU, НЕТ - ENG');
+const lang = language===true ? 'ru' : 'en';
+const ruDaysOfWeek = ' Понедельник \n Вторник \n Среда \n Четверг \n Пятница';
+const enDaysOfWeek = ' Monday \n Tuesday \n Wednesday \n Friday \n Saturday';
+let PlusOrMinus = [];
 
-addExpenses = addExpenses.split(',');
-const addExpensesSum = addExpenses.reduce((partialSum,currentValue) => +partialSum + +currentValue ,0);
-const budgetMonth = money - amount1 - amount2 - addExpensesSum; 
-console.log(budgetMonth);
-
-const missionTime = Math.ceil(mission / budgetMonth);
-console.log(missionTime);
-
-const budgetDay = Math.floor(budgetMonth / 30);
-console.log(budgetDay)
-
-if(budgetDay>1200){
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay>= 600 && budgetDay<=1200) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay<600 && budgetDay>=0) {
-    console.log('К сожалению у вас уровень дохода ниже среднего');
-} else {
-    console.log('Что-то пошло не так');
+if (lang === 'ru') {
+    console.log(ruDaysOfWeek);
+    PlusOrMinus.push (ruDaysOfWeek);
+} else if (lang === 'en') {
+    console.log(enDaysOfWeek);
+    PlusOrMinus.push(enDaysOfWeek);
 }
+
+switch (lang) {
+    case 'ru':
+        console.log(ruDaysOfWeek);
+        break;
+    case 'en':
+        console.log(enDaysOfWeek);
+        break;
+}
+
+console.log(String(PlusOrMinus));
+
+// ВТОРОЕ ЗАДАНИЕ 
+
+const namePerson = prompt('Артем или Максим или Другое значение?');
+const maxim = namePerson === 'Максим' ? console.log('преподаватель') : namePerson === 'Артем' ? console.log('директор') : console.log('студент');
