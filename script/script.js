3fe35c67282c8dc6b949883fff3984018e854240
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-05 07:28:01 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,12 +1,10 @@
 const num = 5551;
 const numberArray = Array.from(String(num),Number);
-const size = numberArray.length;
 
 const resultX3 = numberArray.reduce((partialSum,currentvalue) => partialSum * currentvalue);
 console.log(resultX3);
 
-const resultString = Array.from(String(resultX3**3),Number).splice(0,2);
+const resultString = String(Number(resultX3**3)).slice(0,2);
 console.log(resultString);
-
 
 
