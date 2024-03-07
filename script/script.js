c62825c4032579942ddd81ea631198b6250395d0
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-07 15:24:09 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,20 +1,20 @@
 'use strict'
 function zamikanie () {
 const randomNumber = 34;
-aboba()
+aboba();
 function aboba () {
-    let question = prompt('Угадай число от 1 до 100')
+    let question = prompt('Угадай число от 1 до 100');
     if (question > randomNumber) {
-        alert('Загаданное число меньше')
-        aboba ()
+        alert('Загаданное число меньше');
+        aboba ();
     } else if ( question < randomNumber && question != null) {
-        alert('Загаданное число больше')
-        aboba ()
+        alert('Загаданное число больше');
+        aboba ();
     } else if (!isNaN(question) === false) {
-        alert('Введите число')
-        aboba ()
+        alert('Введите число');
+        aboba ();
     }else if (+question === +randomNumber) {
-        alert ('Это верное число!')
+        alert ('Это верное число!');
     }
 }}
-zamikanie()
+zamikanie();
