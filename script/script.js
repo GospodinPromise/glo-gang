8644a8a1c3a2368976cf3cfdcb1e2859c79c6f76
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-08 10:12:55 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,14 +1,15 @@
 const money = 1000;
 const income = 'фриланс';
-const addExpenses = '100,200,300';
+let addExpenses = '100,200,300';
 const deposit =  true;
 const mission = 10000;
 const period = 5;
 
+addExpenses = addExpenses.toLowerCase().split(',')
 console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
-console.log('Период равен', period, 'месяцев.', 'Цель заработать', mission, 'рублей');
-console.log(addExpenses.toLowerCase().split(','));
+console.log(`Период равен ${period} месяцев. Цель заработать ${mission} рублей.`);
+console.log(addExpenses[0],addExpenses[1],addExpenses[2]);
 
 const budgetDay = money / 30;
 console.log(budgetDay);
