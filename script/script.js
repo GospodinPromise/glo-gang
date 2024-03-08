89852011b7efc89126abf605b1960c8810a704d4
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-08 10:18:03 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -23,7 +23,7 @@ console.log(budgetDay)
 
 if(budgetDay>1200){
     console.log('У вас высокий уровень дохода');
-} else if (budgetDay>= 600 && budgetDay<=1200) {
+} else if (budgetDay>= 600) {
     console.log('У вас средний уровень дохода');
 } else if (budgetDay<600 && budgetDay>=0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
