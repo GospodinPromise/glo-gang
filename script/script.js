d3b65042a5d8562f299b917ed1dadda84fbc7d1d
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-11 21:35:34 +0300

    Delete script directory

--- script/script.js
+++
@@ -1,9 +0,0 @@
-const money = 1000;
-const income = 'фриланс';
-const addExpenses = '100, 200, 300';
-const deposit =  true;
-const mission = 10000;
-const period = 5;
-
-alert(income);
-console.log(income);
