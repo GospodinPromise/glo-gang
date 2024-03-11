1f0778fa0991f988446b23e934f814194f1e2f55
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-11 13:04:35 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,50 +1,38 @@
-'use strict'
-let randomNumber = getRandomInt(0,100);
+'use strict';
+daysOfWeek = ['Понедельник','Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье'];
+const ponedelnik = document.querySelector('#pon');
+const vtornik = document.querySelector('#vtr');
+const sreda = document.querySelector('#srd');
+const chetverg = document.querySelector('#cht');
+const pyatnica = document.querySelector('#ptn');
+const sybbota = document.querySelector('#sbt');
+const voskresenie = document.querySelector('#vsk');
 
-function zamikanie () {
-let popitki = 10;
-aboba();
-function aboba () {
-    let question = prompt('Угадай число от 1 до 100');
-    if (question > randomNumber) {
-        popitki -= 1 
-            if (popitki>0){
-                alert('Загаданное число меньше.Осталось попыток ' + popitki);
-                aboba ();
-            } else {daNet(false)}
-    } else if ( question < randomNumber && question != null) {
-        popitki -= 1 
-            if (popitki>0){
-                alert('Загаданное число больше.Осталось попыток ' + popitki);
-                aboba ();
-            } else {daNet(false)}
-    } else if (!isNaN(question) === false) {
-        alert('Введите число');
-        aboba ();
-    }else if (+question === +randomNumber) {
-        alert ('Это верное число!');
-    }else {
-        alert ('Прощальное сообщение');
-    }
-}}
-zamikanie();
+ponedelnik.innerHTML = 'Понедельник';
+vtornik.innerHTML = 'Вторник';
+sreda.innerHTML = 'Среда';
+chetverg.innerHTML = 'Четверг';
+pyatnica.innerHTML = 'Пятница';
+sybbota.innerHTML = 'Суббота';
+voskresenie.innerHTML = 'Воскресенье';
 
-function daNet (a) {
-    if (a === true){
-        let daDa = confirm('Поздравляю! Хотели бы сыграть еще ?');
-        if (daDa=== true){
-            zamikanie();
-        }
-    } else if (a === false) {
-        let daDa = confirm('Попытки закончились, хотите сыграть еще?');
-        if (daDa === true){
-            zamikanie();
-        }
-    }
-}
+sybbota.classList.add('text');
+voskresenie.classList.add('text');
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+let datamoya = new Date().getDay();
+
+if (datamoya === 0) {
+    voskresenie.classList.add('jirni');
+} else if(datamoya ===1) {
+    ponedelnik.classList.add('jirni');
+} else if (datamoya===2) {
+    vtornik.classList.add('jirni');
+} else if (datamoya===3) {
+    sreda.classList.add('jirni');
+} else if (datamoya===4){
+    chetverg.classList.add('jirni');
+} else if (datamoya===5) {
+    pyatnica.classList.add('jirni');
+} else if (datamoya===6) {
+    sybbota.classList.add('jirni');
 }
