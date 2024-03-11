cdcca9f93e1855cfe9a11ebae3e3db5b1ede0e16
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-11 21:39:10 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,77 +1,94 @@
-const perviPi = document.querySelector('.pervipi')
-const vtoroiiPi = document.querySelector('.vtoroipi')
+const perviPi = document.querySelector(".pervipi");
+const vtoroiiPi = document.querySelector(".vtoroipi");
 
-
-function abobys (){
-    vtoroiiPi.innerHTML = new Date().toLocaleString()
+function abobys() {
+    vtoroiiPi.innerHTML = new Date().toLocaleString();
 }
-abobys()
-setInterval(abobys,1000)
-
+abobys();
+setInterval(abobys, 1000);
 
 let d = new Date();
-const monthA = 'января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря'.split(',');
-monthA[d.getMonth()]
-const dayOfWeek = 'воскресенье,понедельник,вторник,среда,четверг,пятница,суббота'.split(',')
-dayOfWeek[d.getDay()]
+const monthA = "января,февраля,марта,апреля,мая,июня,июля,августа,сентября,октября,ноября,декабря".split(",");
+monthA[d.getMonth()];
+const dayOfWeek = "воскресенье,понедельник,вторник,среда,четверг,пятница,суббота".split(",");
+dayOfWeek[d.getDay()];
 
-
-function vabobys () {
-    perviPi.innerHTML = 'Cегодня ' + dayOfWeek[d.getDay()] + ',' + new Date().getDate() + ' ' + monthA[d.getMonth()] + ' ' +  new Date().getFullYear() + ', ' + d.getHours() + ' ' + vitalcho()+ ' ' + new Date().getMinutes()+ ' ' +vitalcho2()+ ' '+ new Date().getSeconds()+ ' ' + vitalcho3()
+function vabobys() {
+    perviPi.innerHTML =
+        "Cегодня " +
+        dayOfWeek[d.getDay()] +
+        "," +
+        new Date().getDate() +
+        " " +
+        monthA[d.getMonth()] +
+        " " +
+        new Date().getFullYear() +
+        ", " +
+        d.getHours() +
+        " " +
+        vitalcho() +
+        " " +
+        new Date().getMinutes() +
+        " " +
+        vitalcho2() +
+        " " +
+        new Date().getSeconds() +
+        " " +
+        vitalcho3();
 }
-vabobys()
-setInterval(vabobys,1000)
+vabobys();
+setInterval(vabobys, 1000);
 
-function vitalcho () {
-    if (new Date().getHours() === 1){
-        return 'Час'
+function vitalcho() {
+    if (new Date().getHours() === 1) {
+        return "Час";
     } else if (new Date().getHours() < 5) {
-        return 'Часа'
+        return "Часа";
     } else if (new Date().getHours() === 21) {
-        return 'Час'
-    } else if (new Date().getHours() > 21){
-        return 'Часа'
-    } else  {
-        return 'Часов'
+        return "Час";
+    } else if (new Date().getHours() > 21) {
+        return "Часа";
+    } else {
+        return "Часов";
     }
 }
 
 function vitalcho2() {
-    let popka = new Date().getMinutes() 
-    popka = popka + ''
-    if (popka[1]==='1'){
-        return 'Минута'
-    }else if (popka[1] < 5 && popka[1] != 0){
-        return 'Минуты'} 
-    else if (popka.length < 2){
-        if (popka[0] === '1'){
-            return 'Минута'
+    let popka = new Date().getMinutes();
+    popka = popka + "";
+    if (popka[1] === "1") {
+        return "Минута";
+    } else if (popka[1] < 5 && popka[1] != 0) {
+        return "Минуты";
+    } else if (popka.length < 2) {
+        if (popka[0] === "1") {
+            return "Минута";
         } else if (popka[0] < 5 && popka[0] != 0) {
-            return 'Минуты'
+            return "Минуты";
         } else {
-            return 'Минут'
+            return "Минут";
         }
     } else {
-        return 'Минут'
+        return "Минут";
     }
 }
 
 function vitalcho3() {
-    let popka = new Date().getSeconds() 
-    popka = popka + ''
-    if (popka[1]==='1'){
-        return 'Cекунда'
-    }else if (popka[1] < 5 && popka[1] != 0){
-        return 'Секунды'} 
-    else if (popka.length < 2){
-        if (popka[0] === '1'){
-            return 'Секунда'
+    let popka = new Date().getSeconds();
+    popka = popka + "";
+    if (popka[1] === "1") {
+        return "Cекунда";
+    } else if (popka[1] < 5 && popka[1] != 0) {
+        return "Секунды";
+    } else if (popka.length < 2) {
+        if (popka[0] === "1") {
+            return "Секунда";
         } else if (popka[0] < 5 && popka[0] != 0) {
-            return 'Секунды'
+            return "Секунды";
         } else {
-            return 'Секунд'
+            return "Секунд";
         }
     } else {
-        return 'Секунд'
+        return "Секунд";
     }
 }
