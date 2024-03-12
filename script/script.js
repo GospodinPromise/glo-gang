ba58870f1a64238b87c870244cda24aa6d6c49a8
GospodinPromise <158532470+GospodinPromise@users.noreply.github.com>
2024-03-12 20:49:59 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,19 @@
-const money = 1000;
-const income = 'фриланс';
-const addExpenses = '100, 200, 300';
-const deposit =  true;
-const mission = 10000;
-const period = 5;
+'use strict'
+const inputLag = document.querySelector('#input')
+const button = document.querySelector('#btn')
+const spisok = document.querySelector('.spisok')
 
-alert(income);
-console.log(income);
+button.addEventListener('click', function(aboba) {
+    if (inputLag.value.length != 0) {
+    let lishka = document.createElement('li')
+    lishka.textContent = inputLag.value
+    spisok.insertAdjacentElement('beforeend',lishka)
+    inputLag.value = ''
+} else if (inputLag.value.length === 0){
+    inputLag.style.backgroundColor = 'red'
+}
+})
+
+inputLag.addEventListener('focus', function() {
+    inputLag.style.backgroundColor = 'white'
+})
